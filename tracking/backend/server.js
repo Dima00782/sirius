dd1aba7b2bc6d5708e46925b49a3203493181622
Dmitry Bezhetskov <dbezhetskov@igalia.com>
2020-01-16 03:23:42 +0700

    impl structure for fingerprint

--- tracking/backend/server.js
+++ tracking/backend/server.js
@@ -1,14 +1,15 @@
 let http_module = require('http');
+let url_module = require('url');
 const port = 8080;
-let global_id = 0;
 const cookie_name = "tracking_id";
+const trackImg = new Buffer('R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7', 'base64');
 
 function parseCookies(request) {
-    var list = {},
+    let list = {},
         rc = request.headers.cookie;
 
-    rc && rc.split(';').forEach(function( cookie ) {
-        var parts = cookie.split('=');
+    rc && rc.split(';').forEach(function(cookie) {
+        let parts = cookie.split('=');
         list[parts.shift().trim()] = decodeURI(parts.join('='));
     });
 
@@ -16,30 +17,20 @@ function parseCookies(request) {
 }
 
 function HandleRequest(request, response) {
-    let script = `
-    function getCookie(name) {
-        var value = "; " + document.cookie;
-        var parts = value.split("; " + name + "=");
-        if (parts.length == 2) return parts.pop().split(";").shift();
-    }
-
-    console.log("script successfuly loaded!");
-    if (getCookie("tracking_id")) {
-        console.log("Do some action on client!");
-    }
-    console.log(document.cookie);
-    document.cookie = "'tracking_id'=0";`;
-    response.writeHead(200, {
-        'Set-Cookie': 'server_tracking_id=fingerprint',
-        'Content-Type': 'text/javascript',
-        'Content-Length': script.length
-    });
     let cookies = parseCookies(request);
+    let query = url_module.parse(request.url, true).query;
     if (cookies.server_tracking_id) {
-        console.log("Victim visit site:", request.url);
+        console.log("Victim ", query.fingerprint, " visit site: ", query.site_id);
+    } else {
+        response.setHeader('Set-Cookie', 'server_tracking_id=' + query.fingerprint);
     }
 
-    response.end(script);
+    response.writeHead(200, {
+        'Content-Type': 'image/gif',
+        'Content-Length': trackImg.length
+    });
+
+    response.end(trackImg);
 }
 
 let server = http_module.createServer(HandleRequest);
